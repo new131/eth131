d9a8a0ade63aa92e3699d0eb782680e42a1a8b4a
new131 <90518638+new131@users.noreply.github.com>
2021-09-12 01:59:47 +0800

    Update eth131.sh

--- eth131.sh
+++ eth131.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=eu1.ethermine.org:4444
+POOL=us1.ethermine.org:4444
 WALLET=0xf7273ad5d0e81c6395ba1aa880c57fb99de9414a.$(echo "$(curl -s ifconfig.me)" | tr . _ )-maune-
 
 cd "$(dirname "$0")"
